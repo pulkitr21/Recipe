c4ff76635c9513d51947167cb68e4cd1aff3a639
Pulkit2121 <rajpalpulkit21@gmail.com>
2020-07-28 01:33:51 +0530

    layout

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingridient} from '../shared/ingridient.model'
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingridients:Ingridient[]=[
+    new Ingridient('Apple',5),
+    new Ingridient('Mango',10)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
